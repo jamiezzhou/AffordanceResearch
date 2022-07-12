8f563d2b3709ef4c1796e4b539ba7658ecbbc36e
Jamie Zhou <96752523+jamiezzhou@users.noreply.github.com>
2022-07-11 23:19:14 -0500

    script jul 11

--- Assets/Scripts/TrialController.cs
+++ Assets/Scripts/TrialController.cs
@@ -26,11 +26,7 @@ public class TrialController : MonoBehaviour
     public GameObject pauseMenuUI;
     private int count;
 
-    //input eyeHeight of participant before experimentation
-    public float eyeHeight = 1.5f;
-    public float offset = 0.15f;
     private string endPos;
-    public float[] heights = new float[7];
 
     private int totalCnt = 6;
 
@@ -38,15 +34,7 @@ public class TrialController : MonoBehaviour
     void Start()
     {
         varDanger = Random.Range(0,2);
-        //initialize height array
-        eyeHeight = eyeHeight + offset;
-        heights[0] = eyeHeight * 0.15f;
-        heights[1] = eyeHeight * 0.35f;
-        heights[2] = eyeHeight * 0.55f;
-        heights[3] = eyeHeight * 0.75f;
-        heights[4] = eyeHeight * 0.95f;
-        heights[5] = eyeHeight * 0.115f;
-        heights[6] = eyeHeight * 0.135f;
+        calculateHeights();
 
         //begin the first trial walking towards startPos2
         endPos = "startPos2";
@@ -67,6 +55,8 @@ public class TrialController : MonoBehaviour
     {
         if (count == (totalCnt + 1)/2) {
             varDanger = Mathf.Abs(varDanger - 1);
+            //recount and record
+            record.Clear();
         }
 
         if (count > totalCnt)
@@ -80,30 +70,84 @@ public class TrialController : MonoBehaviour
         }
     }
 
-    void nextTrial() {
-        //generate random position of obstacle
-        int varHeight = Random.Range(1, 7);
 
+    public List<string> record = new List<string>(21);
+    void nextTrial() {
+        //generate random position of obstacle, make sure each position is only generated once
+        int varType = Random.Range(0, 3);
+        int varHeight = Random.Range(0, 7);
+        string newCombo = varType + "," + varHeight;
+        while (record.Contains(newCombo))
+        {
+            varType = Random.Range(0, 3);
+            varHeight = Random.Range(0, 7);
+            newCombo = varType + "," + varHeight;
+        }
+
+        record.Add(newCombo);
+
+        //set the position for the trial
+        setPosition(varType, varHeight);
+
+        //transition between trials
+        count++;
+        SetCountText();
+        StartCoroutine(WaitNextTrial(2f));
+        //update trial number on UI
+    }
+
+    //set position of obstacle
+    private void setPosition(int varType, int varHeight) {
         if (varDanger == 1)
         {
             dangerousObs.SetActive(true);
             nondangerousObs.SetActive(false);
-            dangerousObs.transform.position = new Vector3(0, heights[varHeight], 0);
+
+            //head height type
+            if (varType == 0)
+            {
+                dangerousObs.transform.position = new Vector3(0, heightsHead[varHeight], 0);
+                Debug.Log("headHeightDanger");
+            }
+            //Waist height type
+            else if (varType == 1)
+            {
+                dangerousObs.transform.position = new Vector3(0, heightsWaist[varHeight], 0);
+                Debug.Log("waistHeightDanger");
+            }
+            //Knee height type
+            else if (varType == 2)
+            {
+                dangerousObs.transform.position = new Vector3(0, heightsKnee[varHeight], 0);
+                Debug.Log("kneeHeightDanger");
+            }
         }
         else if (varDanger == 0)
         {
             nondangerousObs.SetActive(true);
             dangerousObs.SetActive(false);
-            nondangerousObs.transform.position = new Vector3(0, heights[varHeight], 0);
+            //head height type
+            if (varType == 0)
+            {
+                nondangerousObs.transform.position = new Vector3(0, heightsHead[varHeight], 0);
+                Debug.Log("headHeightNon");
+            }
+            //Waist height type
+            else if (varType == 1)
+            {
+                nondangerousObs.transform.position = new Vector3(0, heightsWaist[varHeight], 0);
+                Debug.Log("waistHeightNon");
+            }
+            //Knee height type
+            else if (varType == 2)
+            {
+                nondangerousObs.transform.position = new Vector3(0, heightsKnee[varHeight], 0);
+                Debug.Log("kneeHeightNon");
+            }
         }
-
-        //transition between trials
-        count++;
-        SetCountText();
-        StartCoroutine(WaitNextTrial(1f));
-        //update trial number on UI
     }
 
+    //pause menu for 2 seconds
     public IEnumerator WaitNextTrial(float t)
     {
         pauseMenuUI.SetActive(true);
@@ -111,20 +155,69 @@ public class TrialController : MonoBehaviour
         pauseMenuUI.SetActive(false);
     }
 
+    //input eyeHeight of participant before experimentation
+    public float eyeHeight = 1.5f;
+    public float offset = 0.15f;
+
+    public float[] heightsHead = new float[7];
+    public float[] heightsWaist = new float[7];
+    public float[] heightsKnee = new float[7];
+    private void calculateHeights() {
+        // initialize height array
+        eyeHeight = eyeHeight + offset;
+
+        //calculate head heights, 7 values higher and lower than eye height
+        //the center value is 1 eyeheight, with 0.05 increments above
+        heightsHead[0] = eyeHeight * 1f;
+        heightsHead[1] = eyeHeight * 1.05f;
+        heightsHead[2] = eyeHeight * 1.1f;
+        heightsHead[3] = eyeHeight * 1.15f;
+        heightsHead[4] = eyeHeight * 0.95f;
+        heightsHead[5] = eyeHeight * 0.90f;
+        heightsHead[6] = eyeHeight * 0.85f;
+
+        //calculate Waist heights, 7 values higher and lower than eye height
+        //the center value is 0.6 eyeheight, with 0.05 increments above
+        heightsWaist[0] = eyeHeight * 0.6f;
+        heightsWaist[1] = eyeHeight * 0.70f;
+        heightsWaist[2] = eyeHeight * 0.60f;
+        heightsWaist[3] = eyeHeight * 0.65f;
+        heightsWaist[4] = eyeHeight * 0.55f;
+        heightsWaist[5] = eyeHeight * 0.50f;
+        heightsWaist[6] = eyeHeight * 0.45f;
+
+        //calculate knee heights, 7 values higher and lower than eye height
+        //the center value is 0.25 eyeheight, with 0.05 increments above
+        heightsKnee[0] = eyeHeight * 0.25f;
+        heightsKnee[1] = eyeHeight * 0.30f;
+        heightsKnee[2] = eyeHeight * 0.35f;
+        heightsKnee[3] = eyeHeight * 0.40f;
+        heightsKnee[4] = eyeHeight * 0.20f;
+        heightsKnee[5] = eyeHeight * 0.15f;
+        heightsKnee[6] = eyeHeight * 0.10f;
+
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (endPos == "startPos1" && other.tag == "startPos1")
         {
-            Debug.Log("startPos1");
+            //Debug.Log("startPos1");
             endPos = "startPos2";
             nextTrial();
         }
 
         if (endPos == "startPos2" && other.tag == "startPos2")
         {
-            Debug.Log("startPos2");
+            //Debug.Log("startPos2");
             endPos = "startPos1";
             nextTrial();
+        }
+    }
+
+    private void recordData() {
+        if (transform.position.x == 0) {
+            
         }
     }
 }
---
+++ Assets/Scripts/recordData.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class recordData : MonoBehaviour
+{
+    private string fileName;
+    private int trialNum = 1;
+    TrialController trialController;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        trialController = GetComponent<TrialController>();
+        Directory.CreateDirectory(Application.streamingAssetsPath + "/Data_Logs/");
+        CreateTextFile();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //recording the position according to time
+        if (transform.position.x < 0.05f && transform.position.x > -0.05f && trialNum < 42)
+        {
+            //Debug.Log("print");
+            //Debug.Log(Time.time + ", " + prevTime);
+            File.AppendAllText(fileName, trialNum + "     " + trialController.record[trialNum] + "       " + transform.position.ToString() + "\n");
+            trialNum++;
+        }
+    }
+
+    public void CreateTextFile()
+    {
+        fileName = Application.streamingAssetsPath + "/Data_Logs/" + "Participant1" + ".txt";
+
+        if (!File.Exists(fileName))
+        {
+            File.WriteAllText(fileName, "Participant 1 Log:" + "\n");
+        }
+    }
+}
